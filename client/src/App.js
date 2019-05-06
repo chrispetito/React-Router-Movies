ed7e1451500b948c8ddb32d757ec21c53f02321e
chrispetito <christopherpetito@gmail.com>
2019-05-06 13:02:54 -0700

    initial commit; add router tags to index.js, routes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
+import { Route } from 'react-router-dom';
 
 export default class App extends Component {
   constructor() {
@@ -23,6 +24,8 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
         <div>Replace this Div with your Routes</div>
+        <Route component={MovieList} exact path='/'></Route>
+        <Route component={MovieList} exact path='/movies/:id'></Route>
       </div>
     );
   }
